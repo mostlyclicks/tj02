5e516cd65aaab30c845a3780b5ed108a9048f7fc
Carlos Interone <carlos@mostlyclicks.com>
2014-08-01 10:15:56 -0500

    connect assets, css and js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,15 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
+//= require jquery.min
+//= require jquery.easing.1.3.min
+//= require bootstrap.min
+//= require jquery.flexslider-min
+//= require jquery.mixitup.min
+//= require wow.min
+//= require jquery.stellar.min
+//= require jquery.sticky
+//= require bootstrap-hover-dropdown.min
 //= require jquery_ujs
+//= require custom
 //= require_tree .
