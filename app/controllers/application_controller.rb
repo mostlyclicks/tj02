35b4cecc96c3e468a0f67108f5da846a03840521
Carlos Interone <carlos@mostlyclicks.com>
2014-09-05 09:44:08 -0500

    continue design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_filter :load_graphs
+  before_filter :load_graphs, :load_latest_graphs
   protect_from_forgery
 
 
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
 
   def load_graphs
     @graphs = Refinery::Graphs::Graph.all
+  end
+
+  def load_latest_graphs
+    @latest_graphs = Refinery::Graphs::Graph.limit(3)
   end
 
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def main_nav
+    presenter = Refinery::Pages::MenuPresenter.new(refinery_menu_pages, self)
+    presenter.menu_tag = "div class=\"collapse navbar-collapse\" id=\"bs-example-navbar-collapse-1\""
+    presenter.list_tag = "ul class=\"nav navbar-nav navbar-right\""
+    presenter.selected_css = "active"
+    presenter.max_depth = 0
+    presenter
+  end
+
 end
